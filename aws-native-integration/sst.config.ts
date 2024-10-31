50b002a4b4987aa947fcfe580b0ec3d7049e589f
Eduardo Rabelo <oieduardorabelo@gmail.com>
2024-10-31 23:53:58 +1300

    chore: adjust sst names

--- aws-native-integration/sst.config.ts
+++ aws-native-integration/sst.config.ts
@@ -1,42 +1,44 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app(input) {
-    return {
-      name: "aws-lambda-opentelemetry-with-adot",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      home: "aws",
-    };
-  },
-  async run() {
-    const AWS_REGION = process.env.AWS_REGION;
+	app(input) {
+		return {
+			name: "aws-native-integration",
+			removal: input?.stage === "production" ? "retain" : "remove",
+			home: "aws",
+		};
+	},
+	async run() {
+		const AWS_REGION = process.env.AWS_REGION;
 
-    new sst.aws.Function("MyFunction", {
-      handler: "functions/hello.handler",
-      architecture: "x86_64",
-      layers: [`arn:aws:lambda:${AWS_REGION}:901920570463:layer:aws-otel-nodejs-amd64-ver-1-18-1:4`],
-      environment: {
-        AWS_LAMBDA_EXEC_WRAPPER: "/opt/otel-handler",
-      },
-      nodejs: {
-        format: "cjs",
-        esbuild: {
-          external: ["@opentelemetry/api"],
-        },
-      },
-      transform: {
-        function: {
-          tracingConfig: {
-            mode: "Active",
-          },
-        },
-        role: {
-          managedPolicyArns: [
-            "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
-            "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess",
-          ],
-        },
-      },
-    });
-  },
+		new sst.aws.Function("MyFunction", {
+			handler: "functions/hello.handler",
+			architecture: "x86_64",
+			layers: [
+				`arn:aws:lambda:${AWS_REGION}:901920570463:layer:aws-otel-nodejs-amd64-ver-1-18-1:4`,
+			],
+			environment: {
+				AWS_LAMBDA_EXEC_WRAPPER: "/opt/otel-handler",
+			},
+			nodejs: {
+				format: "cjs",
+				esbuild: {
+					external: ["@opentelemetry/api"],
+				},
+			},
+			transform: {
+				function: {
+					tracingConfig: {
+						mode: "Active",
+					},
+				},
+				role: {
+					managedPolicyArns: [
+						"arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+						"arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess",
+					],
+				},
+			},
+		});
+	},
 });
